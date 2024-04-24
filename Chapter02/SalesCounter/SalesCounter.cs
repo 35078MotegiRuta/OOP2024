f3a04bfefeac615ead55720dd2deb66bc8b90875
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-04-24 14:08:17 +0900

    メソッドの移動

--- Chapter02/SalesCounter/SalesCounter.cs
+++ Chapter02/SalesCounter/SalesCounter.cs
@@ -1,27 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SalesCounter {
+    //売上集計クラス
     public class SalesCounter {
         private List<Sale> _sales;
+
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePass) {
+            _sales = ReadSales(filePass);
+        }
+
+        //売上データを読み込み、Saleオブジェクトのリストを返す。
+        private List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
         }
 
         //店舗別売上を求める
         public Dictionary<string, int> GetPerStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach (Sale sale in _sales) {
-                if (dict.ContainsKey(sale.ShopName))
-                    dict[sale.ShopName] += sale.Amount;
+                if (dict.ContainsKey(sale.ProductCategory))
+                    dict[sale.ProductCategory] += sale.Amount;
                 else
-                    dict[sale.ShopName] = sale.Amount;
+                    dict[sale.ProductCategory] = sale.Amount;
             }
             return dict;
         }
