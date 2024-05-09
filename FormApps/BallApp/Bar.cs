5218b704dcb8fa65df42c1127b4f6fd67ef2704e
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-05-09 15:59:56 +0900

    バー反射

--- FormApps/BallApp/Bar.cs
+++ FormApps/BallApp/Bar.cs
@@ -13,14 +13,22 @@ namespace BallApp {
             MoveY = 0;
         }
 
-        public override bool Move() {
+        public override bool Move(PictureBox pbBar, PictureBox pbBall) {
+            Rectangle rBar = new Rectangle(pbBar.Location.X, pbBar.Location.Y,
+                                                pbBar.Width, pbBar.Height);
+
+            Rectangle rBall = new Rectangle(pbBall.Location.X, pbBall.Location.Y,
+                                                pbBall.Width, pbBall.Height);
+
             return true;
         }
 
         public override bool Move(Keys direction) {
             if (direction == Keys.Right) {
+                if(PosX < 635)
                 PosX += MoveX;
             } else if (direction == Keys.Left) {
+                if(PosX > 0)
                 PosX -= MoveX;
             }
             return true;    
