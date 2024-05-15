4294d72c272285b26990b897517325c16c20dbe5
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-05-15 10:34:55 +0900

    偶数かどうか＆奇数かどうか

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,15 +8,32 @@ namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            int count = Count(numbers,5);
+
+            //Judgement judge = IsEven;
+            Judgement judge = IsNotEven;
+
+            int count = Count(numbers, judge);
             Console.WriteLine(count);
         }
 
-        public static int Count(int[] numbers,int num) {
+        //nが偶数かどうかを調べる
+        public static bool IsEven(int n) {
+            return n % 2 == 0; //偶数だとtureが返却される
+        }
+
+        //nが偶数かどうかを調べる
+        public static bool IsNotEven(int n) {
+            return n % 2 == 1; //偶数だとtureが返却される
+        }
+
+
+        public delegate bool Judgement(int value);//デリゲートの宣言
+
+        public static int Count(int[] numbers, Judgement judge) {
             
             int count = 0;
             foreach (var n in numbers) {
-                if (n == num)
+                if (judge(n) == true)
                     count++;
             }
             return count;
