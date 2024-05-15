abb8e96b38e296942a240d6d790fc3d18893a64f
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-05-15 11:49:31 +0900

    ｐ８７まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -9,19 +9,13 @@ namespace Section01 {
         static void Main(string[] args) {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
 
-            var count = Count(numbers, n => n % 2 == 0);
-            Console.WriteLine(count);
+            //int count = numbers.Count(n => n % 2 == 0);
+            double num = numbers.Where(n => n > 5).Average();
+            int total = numbers.Where(n => n > 5).Sum();
+            Console.WriteLine(num);
+            Console.WriteLine(total);
         }
 
-        public static int Count(int[] numbers, Predicate<int> judge) {
-            
-            int count = 0;
-            foreach (var n in numbers) {
-                if (judge(n) == true)
-                    count++;
-            }
-            return count;
-        }
 
     }
 }
