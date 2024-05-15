4b0978566a2026891b41de3ab74a00e969964b96
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-05-15 10:09:28 +0900

    汎用性なし

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            int count = Count(5);
+
+        }
+
+        public static int Count(int num) {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            int count = 0;
+            foreach (var n in numbers) {
+                if (n == num)
+                    count++;
+            }
+            return count;
+        }
+
+    }
+}
