1f3eb48c0cb707da6ec2804bf9d5ac337c791051
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-05-29 15:35:15 +0900

    第５章　問題5.1

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("一つ目の文字列を入力してください:");
+            var str1 = Console.ReadLine();
+            Console.Write("二つ目の文字列を入力してください:");
+            var str2 = Console.ReadLine();
+            if (String.Compare(str1, str2, true) == 0) {
+                Console.WriteLine("等しいです");
+            }else{
+                Console.WriteLine("等しくないです"); 
+            }
+        }
+    }
+}
