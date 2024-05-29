61e926c76c50f2b9c39e3829e195074ac19f7def
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-05-29 15:58:58 +0900

    5.2　途中まで

--- Chapter05/演習問題/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
+            Console.Write("数字文字列を入力してください:");
+            var numStr = Console.ReadLine();
+            if(int.TryParse(numStr, out int num)) {
+                Console.WriteLine(num.ToString("#,0"));
+            }
         }
     }
 }
