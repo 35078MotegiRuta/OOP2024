748380c3778a46dfcc9c945c5191f55f2424df57
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-06-06 15:43:33 +0900

    演習問題　メニューの2:検索以外

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -12,7 +12,7 @@ namespace Section01 {
             var kenchou = new Dictionary<string, string>();
 
             for (int i = 0; i < 5; i++) {
-                Console.WriteLine($"{i+1}件目");
+                Console.WriteLine($"{i + 1}件目");
                 Console.Write("都道府県:");
                 string key = Console.ReadLine();
                 Console.Write("県庁所在地: ");
@@ -20,9 +20,38 @@ namespace Section01 {
 
                 kenchou[key] = value;
             }
-            foreach (var x in kenchou) {
-                Console.WriteLine($"{x.Key}の県庁所在地は{x.Value}です");
-            }
+
+            int input;
+
+            do {
+                Console.WriteLine("*メニュー*");
+                Console.WriteLine("1: 一覧表示");
+                Console.WriteLine("2: 検索");
+                Console.WriteLine("9: 終了");
+                Console.WriteLine("----------- ");
+
+                int.TryParse(Console.ReadLine(), out input);
+
+                switch (input) {
+                    case 1:
+                        foreach (var ken in kenchou) {
+                            Console.WriteLine($"{ken.Key}の県庁所在地は{ken.Value}です");
+                        }
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 9:
+                        Console.WriteLine("終了します");
+                        break;
+
+                    default:
+                        Console.WriteLine("数字が違うよ");
+                        break;
+                }
+            } while (input != 9);
         }
     }
 }
