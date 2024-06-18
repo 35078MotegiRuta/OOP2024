f6a3898bfcb3d1716532ebc8052e055897657471
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-06-18 11:45:00 +0900

    設問２_提出

--- TestProject/Test02/Program.cs
+++ TestProject/Test02/Program.cs
@@ -67,47 +67,42 @@ namespace Test02 {
         //問題１　合計値を表示
         //　　　　出力結果【618】
         private static void Exercise01(List<int> numbers) {
-
-
-
+            var sum = numbers.Sum();
+            Console.WriteLine(sum);
         }
 
         //問題２　偶数の最大値を表示
         //　　　　出力結果【94】
         private static void Exercise02(List<int> numbers) {
-
-
-
+            var max = numbers.Where(x => x % 2 == 0).Max();
+            Console.WriteLine(max);
         }
         //問題３　奇数のみを降順に並べて表示（遅延実行とする）
         //　　　　出力結果【91 87 53 35 31 17】
         private static void Exercise03(List<int> numbers) {
-
-
-
+            foreach (var num in numbers.Where(x => x % 2 == 1).OrderByDescending(x => x))
+                Console.Write(num + " "); 
         }
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(List<int> numbers) {
-
-
-
+            foreach(var num in numbers.Where(x => 10 <= x && x <= 50))
+                Console.Write(num + " ");
         }
 
         //問題５　平均年齢を表示
         //　　　　出力結果【30.2才】
         private static void Exercise05(List<Person> persons) {
-            
-
-
+            var average = persons.Average(x => x.Age);
+            Console.WriteLine(average);
         }
 
         //問題６　身長170cm以上の体重平均を表示
         //　　　　出力結果【72.25kg】
         private static void Exercise06(List<Person> persons) {
-
-
+            var average = persons.Where(x => x.Height >= 170).Average(x => x.Weight);
+            Console.WriteLine(average);
 
         }
 
@@ -116,18 +111,21 @@ namespace Test02 {
         //　　　　　　　　　沖田宏一 59kg
         //　　　　　　　　　片山伸介 65kg】
         private static void Exercise07(List<Person> persons) {
-        
-        
-        
+            var pages = persons.Where(x => x.Weight <= 70);
+            foreach (var page in pages)
+                Console.WriteLine("{0} {1}", page.Name, page.Weight);
+
+
         }
 
         //問題８　名前に「山」の漢字が含まれている人全て表示
         //　　　　出力結果【山田隆司
         //　　　　　　　　　片山伸介】
         private static void Exercise08(List<Person> persons) {
-
-
-
+            var book = persons.Where(x => x.Name.Contains("山"));
+            foreach (var b in book) {
+                Console.WriteLine(b.Name);
+            }
         }
     }
 }
