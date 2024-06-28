ebf8967be7348a3375b0a44732d73bb77afe0584
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-06-28 10:23:25 +0900

    画像以外

--- FormApps/CarReportSystem/Form1.cs
+++ FormApps/CarReportSystem/Form1.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Metrics;
 
 namespace CarReportSystem {
     public partial class Form1 : Form {
@@ -16,14 +17,30 @@ namespace CarReportSystem {
         private void btAddReport_Click(object sender, EventArgs e) {
             CarReport carReport = new CarReport {
                 Date = dtpDate.Value,
-
-
-
+                Auther = cbAuther.Text,
+                Maker = GetRadioButtonMaker(),
+                CarName = cbCarName.Text,
+                Report = tbReport.Text,
+                //Picture = pbPicture
             };
             listCarReports.Add(carReport);
+        }
 
-
-
+        //選択されているメーカー名を列挙型で返す
+        private CarReport.MakerGroup GetRadioButtonMaker() {
+            if (rbToyota.Checked == true) {
+                return CarReport.MakerGroup.トヨタ;
+            } else if (rbNissan.Checked == true) {
+                return CarReport.MakerGroup.日産;
+            } else if (rbHonda.Checked == true) {
+                return CarReport.MakerGroup.ホンダ;
+            } else if (rbSubaru.Checked == true) {
+                return CarReport.MakerGroup.スバル;
+            } else if (rbImport.Checked == true) {
+                return CarReport.MakerGroup.輸入車;
+            } else {
+                return CarReport.MakerGroup.その他;
+            }
         }
     }
 }
