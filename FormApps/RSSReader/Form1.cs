87dc6a8845996eba9e6df13167eeeab858790e8d
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-09-04 15:55:02 +0900

    ウェブページ

--- FormApps/RSSReader/Form1.cs
+++ FormApps/RSSReader/Form1.cs
@@ -1,31 +1,87 @@
-Ôªøusing System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Xml.Linq;
+using static System.Formats.Asn1.AsnWriter;
 
-namespace RSSReader {
+namespace BallApp {
     public partial class Form1 : Form {
+
+        private int scoreCount = 0; //ÉXÉRÉAÉJÉEÉìÉgóp
+
+        //ListÉRÉåÉNÉVÉáÉì
+        private List<Obj> balls = new List<Obj>();    //É{Å[ÉãÉCÉìÉXÉ^ÉìÉXäiî[óp
+        private List<PictureBox> pbs = new List<PictureBox>();      //ï\é¶óp
+
+        //ÉoÅ[óp
+        private Bar bar;
+        private PictureBox pbBar;
+
+        //ÉRÉìÉXÉgÉâÉNÉ^
         public Form1() {
             InitializeComponent();
         }
 
-        private void btGet_Click(object sender, EventArgs e) {
-            using (var wc = new WebClient()) {
-                var url = wc.OpenRead(tbRssUrl.Text);
-                XDocument xdoc = XDocument.Load(url);
+        //ÉtÉHÅ[ÉÄÇ™ç≈èâÇ…ÉçÅ[ÉhÇ≥ÇÍÇÈÇ∆Ç´àÍìxÇæÇØé¿çsÇ≥ÇÍÇÈ
+        private void Form1_Load(object sender, EventArgs e) {
+            this.Text = "BallApp SoccerBall:0  TennisBall:0";
 
-                var xtitles = xdoc.Root.Descendants("item").Elements("title").Select(x => x.Value);
-                foreach (var title in xtitles) {
-                    lbRssTitle.Items.Add(title);
+            score.Text = "ÉXÉRÉAÅF" + this.scoreCount;
+
+            bar = new Bar(340, 500);
+            pbBar = new PictureBox();
+
+            pbBar.Image = bar.Image;
+            pbBar.Location = new Point((int)bar.PosX, (int)bar.PosY);
+            pbBar.Size = new Size(150, 10);
+            pbBar.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbBar.Parent = this;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+
+            for (int i = 0; i < balls.Count; i++) {
+                int ret = balls[i].Move(pbBar, pbs[i]);
+                if (ret == 1) {
+                    //óéâ∫ÇµÇΩÉ{Å[ÉãÉCÉìÉXÉ^ÉìÉXÇçÌèúÇ∑ÇÈ 
+                    balls.RemoveAt(i);
+                    pbs[i].Location = new Point(2000, 2000);
+                    pbs.RemoveAt(i);    
+                } else if (ret == 2) {
+                    //ÉoÅ[Ç…ìñÇΩÇ¡ÇΩ
+                    score.Text = "ÉXÉRÉAÅF" + this.scoreCount++;
+                    pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);
+                } else {
+                    //ê≥èÌà⁄ìÆ
+                    pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);
                 }
             }
+        }
+
+        //É}ÉEÉXÉNÉäÉbÉNÉCÉxÉìÉgÉnÉìÉhÉâ
+        private void Form1_MouseClick(object sender, MouseEventArgs e) {
+            PictureBox pb = new PictureBox();   //âÊëúÇï\é¶Ç∑ÇÈÉRÉìÉgÉçÅ[Éã
+            Obj ball = null;
+
+            if (e.Button == MouseButtons.Left) {
+                ball = new SoccerBall(e.X - 25, e.Y - 25);
+                pb.Size = new Size(50, 50);
+
+            } else if (e.Button == MouseButtons.Right) {
+                ball = new TennisBall(e.X - 12, e.Y - 12);
+                pb.Size = new Size(25, 25);
+            }
+            pb.Image = ball.Image;
+            pb.Location = new Point((int)ball.PosX, (int)ball.PosY);
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb.Parent = this;
+            timer1.Start();
+
+            balls.Add(ball);
+            pbs.Add(pb);
+
+            this.Text = "BallApp SoccerBall:" + SoccerBall.Count + "  TennisBall:" + TennisBall.Count;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            bar.Move(e.KeyCode);
+            pbBar.Location = new Point((int)bar.PosX, (int)bar.PosY);
         }
     }
 }
