e2327baf9d320e76a0a68ed6ccc9aece229d55c3
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-09-10 12:09:15 +0900

    RssReader途中

--- FormApps/RSSReader/Form1.Designer.cs
+++ FormApps/RSSReader/Form1.Designer.cs
@@ -25,98 +25,113 @@
         private void InitializeComponent() {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.tbRssUrl = new System.Windows.Forms.TextBox();
+            this.cbRssUrl = new System.Windows.Forms.ComboBox();
+            this.tbRssName = new System.Windows.Forms.TextBox();
             this.lbRssTitle = new System.Windows.Forms.ListBox();
             this.wbRss = new System.Windows.Forms.WebBrowser();
-            this.button1 = new System.Windows.Forms.Button();
             this.btGet = new System.Windows.Forms.Button();
+            this.btJoin = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 19);
+            this.label1.Location = new System.Drawing.Point(12, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 12);
+            this.label1.Size = new System.Drawing.Size(107, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "URLまたはお気に入り名称：";
+            this.label1.Text = "URLまたはカテゴリ名：";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(77, 48);
+            this.label2.Location = new System.Drawing.Point(33, 48);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(86, 12);
             this.label2.TabIndex = 1;
             this.label2.Text = "お気に入り名称：";
             // 
-            // comboBox1
+            // cbRssUrl
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(169, 16);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(538, 20);
-            this.comboBox1.TabIndex = 2;
+            this.cbRssUrl.FormattingEnabled = true;
+            this.cbRssUrl.Location = new System.Drawing.Point(125, 13);
+            this.cbRssUrl.Name = "cbRssUrl";
+            this.cbRssUrl.Size = new System.Drawing.Size(596, 20);
+            this.cbRssUrl.TabIndex = 2;
             // 
-            // tbRssUrl
+            // tbRssName
             // 
-            this.tbRssUrl.Location = new System.Drawing.Point(169, 45);
-            this.tbRssUrl.Name = "tbRssUrl";
-            this.tbRssUrl.Size = new System.Drawing.Size(326, 19);
-            this.tbRssUrl.TabIndex = 3;
+            this.tbRssName.Location = new System.Drawing.Point(125, 45);
+            this.tbRssName.Name = "tbRssName";
+            this.tbRssName.Size = new System.Drawing.Size(412, 19);
+            this.tbRssName.TabIndex = 3;
             // 
             // lbRssTitle
             // 
             this.lbRssTitle.FormattingEnabled = true;
             this.lbRssTitle.ItemHeight = 12;
-            this.lbRssTitle.Location = new System.Drawing.Point(29, 90);
+            this.lbRssTitle.Location = new System.Drawing.Point(12, 73);
             this.lbRssTitle.Name = "lbRssTitle";
-            this.lbRssTitle.Size = new System.Drawing.Size(160, 460);
+            this.lbRssTitle.Size = new System.Drawing.Size(201, 496);
             this.lbRssTitle.TabIndex = 4;
             this.lbRssTitle.SelectedIndexChanged += new System.EventHandler(this.lbRssTitle_SelectedIndexChanged);
             // 
             // wbRss
             // 
-            this.wbRss.Location = new System.Drawing.Point(212, 90);
+            this.wbRss.Location = new System.Drawing.Point(219, 74);
             this.wbRss.MinimumSize = new System.Drawing.Size(20, 20);
             this.wbRss.Name = "wbRss";
             this.wbRss.ScriptErrorsSuppressed = true;
-            this.wbRss.Size = new System.Drawing.Size(560, 460);
+            this.wbRss.Size = new System.Drawing.Size(689, 495);
             this.wbRss.TabIndex = 5;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(727, 14);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "取得";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // btGet
             // 
-            this.btGet.Location = new System.Drawing.Point(543, 45);
+            this.btGet.Location = new System.Drawing.Point(727, 11);
             this.btGet.Name = "btGet";
             this.btGet.Size = new System.Drawing.Size(75, 23);
-            this.btGet.TabIndex = 7;
-            this.btGet.Text = "登録";
+            this.btGet.TabIndex = 6;
+            this.btGet.Text = "取得";
             this.btGet.UseVisualStyleBackColor = true;
             this.btGet.Click += new System.EventHandler(this.btGet_Click);
+            // 
+            // btJoin
+            // 
+            this.btJoin.Location = new System.Drawing.Point(543, 43);
+            this.btJoin.Name = "btJoin";
+            this.btJoin.Size = new System.Drawing.Size(75, 23);
+            this.btJoin.TabIndex = 7;
+            this.btJoin.Text = "登録";
+            this.btJoin.UseVisualStyleBackColor = true;
+            this.btJoin.Click += new System.EventHandler(this.btJoin_Click);
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(833, 10);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(75, 23);
+            this.btDelete.TabIndex = 8;
+            this.btDelete.Text = "削除";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(814, 560);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(920, 581);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.btJoin);
             this.Controls.Add(this.btGet);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.wbRss);
             this.Controls.Add(this.lbRssTitle);
-            this.Controls.Add(this.tbRssUrl);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tbRssName);
+            this.Controls.Add(this.cbRssUrl);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Name = "Form1";
             this.Text = "RssReader";
             this.ResumeLayout(false);
@@ -128,12 +143,13 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox tbRssUrl;
+        private System.Windows.Forms.ComboBox cbRssUrl;
+        private System.Windows.Forms.TextBox tbRssName;
         private System.Windows.Forms.ListBox lbRssTitle;
         private System.Windows.Forms.WebBrowser wbRss;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btGet;
+        private System.Windows.Forms.Button btJoin;
+        private System.Windows.Forms.Button btDelete;
     }
 }
 
--- FormApps/RSSReader/Form1.cs
+++ FormApps/RSSReader/Form1.cs
@@ -1,48 +1,175 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace RssReader {
-
     public partial class Form1 : Form {
-        List<ItemData> items;
+        private List<ItemData> items;
+        private List<CategoryUrlPair> categoryUrlPairs;
+        private List<CategoryUrlPair> defaultCategories;
 
         public Form1() {
             InitializeComponent();
+            InitializeCategoryUrlPairs();
+            MessageBox.Show("①カテゴリ選択またはURLを入力して取得ボタンを押す\n" +
+                "②お気に入り名称とURLを入力して登録ボタンを押す", "使い方", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            cbRssUrl.DropDownStyle = ComboBoxStyle.DropDown;
+            cbRssUrl.SelectedIndex = -1;
+            cbRssUrl.Text = "";
         }
 
-        private void btGet_Click(object sender, EventArgs e) {
-                using (var wc = new WebClient()) {
-                    var url = wc.OpenRead(tbRssUrl.Text);
-                    var xdoc = XDocument.Load(url);
+        private void InitializeCategoryUrlPairs() {
+            categoryUrlPairs = new List<CategoryUrlPair>
+            {
+                new CategoryUrlPair("主要", "https://news.yahoo.co.jp/rss/topics/top-picks.xml"),
+                new CategoryUrlPair("国内", "https://news.yahoo.co.jp/rss/topics/domestic.xml"),
+                new CategoryUrlPair("国際", "https://news.yahoo.co.jp/rss/topics/world.xml"),
+                new CategoryUrlPair("経済", "https://news.yahoo.co.jp/rss/topics/business.xml"),
+                new CategoryUrlPair("エンタメ", "https://news.yahoo.co.jp/rss/topics/entertainment.xml"),
+                new CategoryUrlPair("スポーツ", "https://news.yahoo.co.jp/rss/topics/sports.xml"),
+                new CategoryUrlPair("IT", "https://news.yahoo.co.jp/rss/topics/it.xml"),
+                new CategoryUrlPair("科学", "https://news.yahoo.co.jp/rss/topics/science.xml"),
+                new CategoryUrlPair("地域", "https://news.yahoo.co.jp/rss/topics/local.xml"),
+            };
 
-                    items = xdoc.Root.Descendants("item")
-                                        .Select(item => new ItemData {
-                                            Title = item.Element("title").Value,
-                                            Link = item.Element("link").Value,
-                                        }).ToList();
+            defaultCategories = new List<CategoryUrlPair>(categoryUrlPairs);
 
-                foreach (var item in items) {
-                    lbRssTitle.Items.Add(item.Title);
+            cbRssUrl.DataSource = categoryUrlPairs;
+            cbRssUrl.DisplayMember = "Category";
+            cbRssUrl.ValueMember = "Url";
+        }
+
+        private async void btGet_Click(object sender, EventArgs e) {
+            string selectedUrl;
+
+            if (cbRssUrl.SelectedItem is CategoryUrlPair selectedPair) {
+                selectedUrl = selectedPair.Url;
+            } else {
+                selectedUrl = cbRssUrl.Text;
+            }
+
+            if (!string.IsNullOrWhiteSpace(selectedUrl)) {
+                try {
+                    using (var wc = new WebClient()) {
+                        wc.Encoding = Encoding.UTF8;
+                        var rssData = await wc.DownloadStringTaskAsync(selectedUrl);
+
+                        try {
+                            var xdoc = XDocument.Parse(rssData);
+
+                            items = xdoc.Root.Descendants("item")
+                                            .Select(item => new ItemData {
+                                                Title = item.Element("title").Value,
+                                                Link = item.Element("link").Value,
+                                            }).ToList();
+
+                            lbRssTitle.Invoke(new Action(() => {
+                                lbRssTitle.Items.Clear();
+                                foreach (var item in items) {
+                                    lbRssTitle.Items.Add(item.Title);
+                                }
+                            }));
+                        }
+                        catch (XmlException xmlEx) {
+                            MessageBox.Show($"RSSデータのXML解析エラー: {xmlEx.Message}", "XMLエラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
                 }
+                catch (WebException webEx) {
+                    MessageBox.Show($"正式なURLを入力してください", "URLエラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex) {
+                    MessageBox.Show($"エラーが発生しました: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            } else {
+                MessageBox.Show("URLを入力またはカテゴリを選択してください", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
         private void lbRssTitle_SelectedIndexChanged(object sender, EventArgs e) {
-            wbRss.Navigate(items[lbRssTitle.SelectedIndex].Link);
+            if (lbRssTitle.SelectedIndex >= 0) {
+                var selectedItem = items[lbRssTitle.SelectedIndex];
+                if (!string.IsNullOrEmpty(selectedItem.Link)) {
+                    wbRss.Navigate(selectedItem.Link);
+                }
+            }
         }
 
         public class ItemData {
             public string Title { get; set; }
             public string Link { get; set; }
+        }
+
+        public class CategoryUrlPair {
+            public string Category { get; set; }
+            public string Url { get; set; }
+
+            public CategoryUrlPair(string category, string url) {
+                Category = category;
+                Url = url;
+            }
+        }
+
+        private void btJoin_Click(object sender, EventArgs e) {
+            string categoryName = tbRssName.Text;
+            string url = cbRssUrl.Text;
+
+            if (string.IsNullOrWhiteSpace(categoryName)) {
+                MessageBox.Show("カテゴリ名を入力してください。", "入力エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(url)) {
+                MessageBox.Show("URLを入力してください。", "入力エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var newPair = new CategoryUrlPair(categoryName, url);
+            categoryUrlPairs.Add(newPair);
+
+            cbRssUrl.DataSource = null;
+            cbRssUrl.DataSource = categoryUrlPairs;
+            cbRssUrl.DisplayMember = "Category";
+            cbRssUrl.ValueMember = "Url";
+
+            tbRssName.Text = "";
+            cbRssUrl.Text = "";
+
+            MessageBox.Show(categoryName + "が登録されました。", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btDelete_Click(object sender, EventArgs e) {
+            // コンボボックスで選択されたアイテムを取得
+            var selectedPair = cbRssUrl.SelectedItem as CategoryUrlPair;
+
+            if (selectedPair == null) {
+                MessageBox.Show("削除するアイテムを選択してください。", "入力エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // デフォルト項目かどうかをチェック
+            if (defaultCategories.Contains(selectedPair)) {
+                MessageBox.Show("デフォルトの項目は削除できません。", "削除エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // リストから削除
+            categoryUrlPairs.Remove(selectedPair);
+
+            // コンボボックスのデータソースを更新
+            cbRssUrl.DataSource = null; // 一度データソースをクリア
+            cbRssUrl.DataSource = categoryUrlPairs; // 新しいデータソースを設定
+            cbRssUrl.DisplayMember = "Category";
+            cbRssUrl.ValueMember = "Url";
+
+            // 成功メッセージを表示
+            MessageBox.Show("削除しました。", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
