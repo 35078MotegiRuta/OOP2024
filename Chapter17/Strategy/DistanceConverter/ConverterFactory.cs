554361e306c2fbd19e331e929c21feae5523b44c
MotegiRuta <166583778+35078MotegiRuta@users.noreply.github.com>
2024-10-10 10:34:25 +0900

    17.2完成

--- Chapter17/Strategy/DistanceConverter/ConverterFactory.cs
+++ Chapter17/Strategy/DistanceConverter/ConverterFactory.cs
@@ -13,6 +13,8 @@ namespace DistanceConverter {
             new FeetConverter (),
             new YardConverter (),
             new InchConverter (),
+            new MileConverter (),
+            new KmConverter (),
         };
 
         public static ConverterBase GetInstance(string name) {
